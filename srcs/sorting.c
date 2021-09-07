f3d7e887677d9fa85d864fab353c653aa7e55831
Lynderly Orphan <lorphan@mi-g3.msk.21-school.ru>
2021-09-07 19:31:13 +0300

    started sorting

--- srcs/sorting.c
+++ srcs/sorting.c
@@ -1,32 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   sorting.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/07 16:45:29 by lorphan           #+#    #+#             */
-/*   Updated: 2021/09/07 19:30:45 by lorphan          ###   ########.fr       */
+/*   Created: 2021/09/07 19:27:32 by lorphan           #+#    #+#             */
+/*   Updated: 2021/09/07 19:30:40 by lorphan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
+#include "../push_swap.h"
 
-int	main(int argc, char *argv[])
-{
-	t_stack	*a;
-	t_stack	*b;
-
-	if (argc < 2)
-	{
-		ft_putstr("ERROR\n");
-		exit(1);
-	}
-	a = NULL;
-	b = NULL;
-	fill_stack(&a, argc, argv);
-	print(a);
-	clear(&a);
-	clear(&b);
-	return (0);
-}
