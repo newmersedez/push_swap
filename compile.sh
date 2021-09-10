b9a9698c2f75ee725009593ad56c10d3b94453e7
Lynderly Orphan <lorphan@il-j5.msk.21-school.ru>
2021-09-10 15:48:30 +0300

    new_logic

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -Wall -Wextra -Werror ./libft/*.c ./stack/*.c ./sources/*.c -o push_swap
--- sources/sorting.c
+++
@@ -1,7 +0,0 @@
-#include "push_swap.h"
-
-void	start_sorting(t_stack **a, t_stack **b)
-{
-	if (!a || !*a || !b)
-		handle_error(a);
-}
