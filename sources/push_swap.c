75e97b047e58e76eafaffdeddb9eacdd71f8e26f
Lynderly Orphan <lorphan@il-j3.msk.21-school.ru>
2021-09-11 21:06:31 +0300

    filing is working

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,26 +6,11 @@
 /*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:12:34 by lorphan           #+#    #+#             */
-/*   Updated: 2021/09/11 21:01:54 by lorphan          ###   ########.fr       */
+/*   Updated: 2021/09/11 21:04:56 by lorphan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-#include <stdio.h>
-
-static void	print(t_stack *a)
-{
-	int i;
-	
-	if (!a || a->top_id == -1)
-		return ;
-	i = a->top_id;
-	while (i > -1)
-	{
-		printf("%d\n", a->array[i]);
-		i--;
-	}
-}
 
 int	main(int argc, char *argv[])
 {
@@ -34,11 +19,12 @@ int	main(int argc, char *argv[])
 
 	if (argc < 2 || !argv)
 		exit(EXIT_SUCCESS);
-	a = stack_create(10);
-	b = stack_create(10);
+	a = stack_create(argc - 1);
+	b = stack_create(argc - 1);
 	if (!a || !b || !a->array || !b->array)
 		fail_exit(&a, &b);
-	// fill_stack_with_args(&a, argc, argv);
+	fill_stack_with_args(&a, argc, argv);
+	//sort(&a, &b);
 	success_exit(&a, &b);
 	return (0);
 }
