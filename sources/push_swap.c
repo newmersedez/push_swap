8ebf0485f79591772ec56e4d6e70c807bb0f4d44
newmersedez <trishkk2001@gmail.com>
2021-09-12 23:47:10 +0300

    saving commit

--- sources/push_swap.c
+++ sources/push_swap.c
@@ -11,21 +11,21 @@
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-#include <stdio.h>
-static void	stack_print(t_stack *a)
-{
-	int	i;
+// #include <stdio.h>
+// static void	stack_print(t_stack *a)
+// {
+// 	int	i;
 
-	if (!a)
-		return ;
-	i = a->top_id;
-	while (i >= 0)
-	{
-		printf("%d\n", a->array[i]);
-		i--;
-	}
-	printf("\n\n");
-}
+// 	if (!a)
+// 		return ;
+// 	i = a->top_id;
+// 	while (i >= 0)
+// 	{
+// 		printf("%d\n", a->array[i]);
+// 		i--;
+// 	}
+// 	printf("\n\n");
+// }
 
 int	main(int argc, char *argv[])
 {
@@ -39,9 +39,9 @@ int	main(int argc, char *argv[])
 	if (!a || !b || !a->array || !b->array)
 		fail_exit(&a, &b);
 	fill_stack_with_args(&a, &b, argc, argv);
-	stack_print(a);
+	// stack_print(a);
 	sort(&a, &b);
-	stack_print(a);
+	// stack_print(a);
 	success_exit(&a, &b);
 	return (0);
 }
